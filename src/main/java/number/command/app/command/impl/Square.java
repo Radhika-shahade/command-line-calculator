037bc036b6e76cbf4dfe7e3166f6055459f80746
Radhika shahade <radhikashahade0804@gmail.com>
2023-12-22 21:20:14 +0530

    command line argument project

---
+++ src/main/java/number/command/app/command/impl/Square.java
@@ -0,0 +1,35 @@
+package number.command.app.command.impl;
+
+import number.command.app.command.Command;
+import number.command.app.command.validator.CommandValidator;
+import number.command.app.model.Result;
+
+public class Square implements CommandValidator, Command {
+    @Override
+    public Result execute(String[] numbers) throws Exception {
+        if (validate(numbers)) {
+            for (int i = 1; i < numbers.length; i++) {
+                Double num = Double.valueOf(numbers[1]);
+                Double result = calculateSquare(num);
+                System.out.println(result + "is square of number"+num);
+                }
+            return Result.builder().message("square is calculated ").build();
+            }
+        return Result.builder().message("Invalid attributes ").build();
+        }
+
+    private Double calculateSquare(Double num) {
+        return Math.pow(num,2);
+    }
+
+
+    @Override
+    public boolean validate(String[] attributes) throws Exception {
+        if(!(attributes.length>1))
+        {
+            throw new Exception(" sqaure required at least one numbers");
+        }
+        return true;
+    }
+
+}
---
+++ src/main/java/number/command/app/command/impl/Subtraction.java
@@ -0,0 +1,29 @@
+package number.command.app.command.impl;
+
+import number.command.app.command.Command;
+import number.command.app.command.validator.CommandValidator;
+import number.command.app.model.Result;
+
+public class Subtraction implements Command, CommandValidator {
+    @Override
+    public Result execute(String[] numbers) throws Exception {
+        Double sub = Double.valueOf(numbers[1]);
+        if (validate(numbers)) {
+            for (int i = 2; i < numbers.length; i++) {
+                int num = Integer.valueOf(numbers[i]);
+                sub-= num;
+            }
+            return Result.builder().message("subtraction of the given number is:").value(sub).build();
+        }
+        return Result.builder().message("provide valid attributes").build();
+    }
+
+    @Override
+    public boolean validate(String[] attributes) throws Exception {
+        if(!(attributes.length>2))
+        {
+            throw new Exception(" subtraction required two numbers");
+        }
+        return true;
+    }
+}
