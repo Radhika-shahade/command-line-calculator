7c87d27accb97932748a005ebf4bb05e3311aa64
Radhika shahade <radhikashahade0804@gmail.com>
2023-12-22 21:19:41 +0530

    command line argument project

---
+++ src/main/java/number/command/app/command/impl/Multiplication.java
@@ -0,0 +1,33 @@
+package number.command.app.command.impl;
+
+import number.command.app.command.Command;
+import number.command.app.command.validator.CommandValidator;
+import number.command.app.model.Result;
+
+public class Multiplication implements Command, CommandValidator {
+    @Override
+    public Result execute(String[] numbers) throws Exception {
+        Double mul = 1.0;
+        if (validate(numbers)) {
+            for (int i = 1; i < numbers.length; i++) {
+                String currentNumber = numbers[i];
+                Double number = Double.parseDouble(currentNumber);
+                mul *= number;
+            }
+            return Result.builder().message("Multiplication of given numbers is: ").value(mul).build();
+        }
+        return Result.builder().message("please write integer numbers").build();
+    }
+
+    @Override
+    public boolean validate(String[] attributes) throws Exception {
+        //        mul 10 20 30 -2 34 545
+
+        if(!(attributes.length>2))
+        {
+            throw new Exception(" multiplication required at least two numbers");
+        }
+        return true;
+    }
+
+}
