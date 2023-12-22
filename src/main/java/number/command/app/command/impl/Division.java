2ecf257e3de63ca9dc999bbf0338564753fb546c
Radhika shahade <radhikashahade0804@gmail.com>
2023-12-22 21:18:21 +0530

    command line argument project

---
+++ src/main/java/number/command/app/command/impl/Division.java
@@ -0,0 +1,32 @@
+package number.command.app.command.impl;
+
+import number.command.app.command.Command;
+import number.command.app.command.validator.CommandValidator;
+import number.command.app.model.Result;
+
+public class Division implements Command, CommandValidator {
+    @Override
+    public Result execute(String[] numbers) throws Exception {
+        //div 45 4
+        Double div = Double.parseDouble(numbers[1]);
+      if(validate(numbers))
+      {
+         for(int i=2;i<numbers.length;i++)
+         {
+            Double num = Double.parseDouble(numbers[i]);
+            div /= num ;
+         }
+          return Result.builder().message("Division of given numbers is: ").value(div).build();
+       }
+        return Result.builder().message("attributes are invalid").build();
+    }
+
+    @Override
+    public boolean validate(String[] attributes) throws Exception {
+        if(!(attributes.length>2))
+        {
+            throw new Exception(" division required at least two numbers");
+        }
+        return true;
+    }
+}
